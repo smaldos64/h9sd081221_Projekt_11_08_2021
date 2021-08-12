143c2804e73ff1f893fd88b7ab35ee65f1ca854a
smaldos64 <gudumholm34@gmail.com>
2021-08-12 14:19:42 +0200

    Indsat Division funktion i branch v1.0.0_Fix udfra SW Version : v1.0.0

--- Function.py
+++ Function.py
@@ -1,6 +1,12 @@
 def AddNumbers(Number1, Number2):
     return (Number1 + Number2 + Number1 + Number2)
 
+def DivideNumbers(Number1, Number2):
+    if 0 == Number2:
+        return (0)
+    else:
+        return (Number1 / Number2)
+
 
 #test
 
